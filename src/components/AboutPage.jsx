07a9af271b57b20f1b627a52947ae69a80af12ce
mohit1721 <mohitmandal192@gmail.com>
2025-05-27 01:25:38 +0530

    first commit

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { FaLink, FaShareAlt, FaEdit, FaChartLine } from "react-icons/fa";
+const AboutPage = () => {
+  return (
+    <div className="bg-[#183B4E] lg:px-14 sm:px-8 px-5 min-h-[calc(100vh-64px)] pt-2">
+      <div className="bg-[#183B4E] w-full sm:py-10 py-8  ">
+        <h1 className="sm:text-4xl text-[#DDA853] text-3xl font-bold italic  mb-3">
+          About ClipLink
+        </h1>
+        <p className="text-[#F3F3E0] text-sm  mb-8 xl:w-[60%] lg:w-[70%] sm:w-[80%] w-full ">
+          ClipLink simplifies URL shortening for efficient sharing. Easily
+          generate, manage, and track your shortened links. ClipLink simplifies
+          URL shortening for efficient sharing. Easily generate, manage, and
+          track your shortened links. ClipLink simplifies URL shortening for
+          efficient sharing. Easily generate, manage, and track your shortened
+          links. ClipLink simplifies URL shortening for efficient sharing.
+          Easily generate, manage, and track your shortened links.
+        </p>
+        <div className="space-y-5 xl:w-[60%] lg:w-[70%] sm:w-[80%] w-full ">
+          <div className="flex items-start">
+            <FaLink className="text-blue-500 text-3xl mr-4" />
+            <div>
+              <h2 className="sm:text-2xl font-bold text-[#DDA853]">
+                Simple URL Shortening
+              </h2>
+              <p className="text-[#F3F3E0]  ">
+                Experience the ease of creating short, memorable URLs in just a
+                few clicks. Our intuitive interface and quick setup process
+                ensure you can start shortening URLs without any hassle.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start">
+            <FaShareAlt className="text-green-500 text-3xl mr-4" />
+            <div>
+              <h2 className="sm:text-2xl font-bold text-[#DDA853]">
+                Powerful Analytics
+              </h2>
+              <p className="text-[#F3F3E0] ">
+                Gain insights into your link performance with our comprehensive
+                analytics dashboard. Track clicks, geographical data, and
+                referral sources to optimize your marketing strategies.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start">
+            <FaEdit className="text-purple-500 text-3xl mr-4" />
+            <div>
+              <h2 className="sm:text-2xl font-bold text-[#DDA853]">
+                Enhanced Security
+              </h2>
+              <p className="text-[#F3F3E0] ">
+                Rest assured with our robust security measures. All shortened
+                URLs are protected with advanced encryption, ensuring your data
+                remains safe and secure.
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start">
+            <FaChartLine className="text-red-500 text-3xl mr-4" />
+            <div>
+              <h2 className="sm:text-2xl font-bold text-[#DDA853]">
+                Fast and Reliable
+              </h2>
+              <p className="text-[#F3F3E0] ">
+                Enjoy lightning-fast redirects and high uptime with our reliable
+                infrastructure. Your shortened URLs will always be available and
+                responsive, ensuring a seamless experience for your users.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
